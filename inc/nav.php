cc473591bbeda1d9c6fc501ff7304e4075328f5f
TonyBall <ball.boy@hotmail.com>
2016-02-23 14:49:44 +0700

    init
    eiei

---
+++ inc/nav.php
@@ -0,0 +1,41 @@
+<!-- Compiled and minified CSS -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+<Link type="text/css" rel="stylesheet" href="style/style.css"/>
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="http://materializecss.com/templates/starter-template/js/init.js"></script>
+
+
+
+<!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="#!">one</a></li>
+  <li><a href="#!">two</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">three</a></li>
+</ul>
+<nav>
+  <div class="nav-wrapper">
+    <a href="#!" class="brand-logo">Logo</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="sass.html">Sass</a></li>
+      <li><a href="badges.html">Components</a></li>
+      <!-- Dropdown Trigger -->
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+    </ul>
+  </div>
+</nav>
+ 
+
+<script>
+ $(".button-collapse").sideNav();
+	$( document ).ready(function(){
+		$(".dropdown-button").dropdown();
+	alert("ball");
+	})
+
+</script>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KINNAREE</title>
+	
+</head>
+<body>
+	<?php include 'inc/nav.php';?>
+	<p>KINNAREE</p>
+	  <div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">Card Title</span>
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+	<?php include 'inc/footer.php';?>
+
+
+	<script>
+		$( document ).ready(function(){
+			$(".button-collapse").sideNav();
+			$(".dropdown-button").dropdown();
+
+		})
+	</script>
+
+</body>
+</html>
